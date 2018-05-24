3023020c432551efe8f71299b73f74bc4276e162
18869978285 <18869978285@163.com>
2018-05-24 18:22:30 +0800

    main

--- app/src/main/java/codec/axun/com/tvdemo/MainActivity.java
+++ app/src/main/java/codec/axun/com/tvdemo/MainActivity.java
@@ -1,93 +1,38 @@
 package codec.axun.com.tvdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.view.View;
 import android.widget.RelativeLayout;
 
-import codec.axun.com.tvdemo.widget.MetroViewBorderImpl;
+import org.evilbinary.tv.widget.BorderView;
 
-public class MainActivity extends Activity{
-
-    private RelativeLayout mRlFirst;
-    private RelativeLayout mRlSecond;
-    private ConstraintLayout mClParent;
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        initView();
 
-        FrameLayout roundedFrameLayout = new FrameLayout(this);
-        roundedFrameLayout.setClipChildren(false);
 
-        final MetroViewBorderImpl metroViewBorderImpl = new MetroViewBorderImpl(roundedFrameLayout);
-        metroViewBorderImpl.setBackgroundResource(R.drawable.border_color);
+        BorderView border=new BorderView(this);
+        border.setBackgroundResource(R.drawable.border_color);
 
-        ViewGroup list = (ViewGroup) findViewById(R.id.list);
-        metroViewBorderImpl.attachTo(list);
+        RelativeLayout main= (RelativeLayout) findViewById(R.id.list);
+        border.attachTo(main);
+        for (int i =0;i<main.getChildCount();i++){
+
+            main.getChildAt(i).setOnClickListener(this);
+        }
     }
 
-//    private void initView() {
-//
-//        mClParent = (ConstraintLayout) findViewById(R.id.cl_parent);
-//        for (int i=0;i<mClParent.getChildCount();i++){
-//            mClParent.getChildAt(i).setFocusable(true);
-//            mClParent.getChildAt(0).requestFocus();
-//            mClParent.getChildAt(i).setOnFocusChangeListener(this);
-//        }
-//
-//    }
-//
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        switch (event.getKeyCode()) {
-//            case KeyEvent.KEYCODE_DPAD_DOWN:
-//                mClParent.getChildAt(0).setNextFocusDownId(R.id.rl_second);
-////                mRlSecond.requestFocus();
-//                break;
-//            case KeyEvent.KEYCODE_DPAD_UP:
-//                mClParent.getChildAt(1).setNextFocusUpId(R.id.rl_first);
-//                break;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//
-//    }
-//
-//    @Override
-//    public void onFocusChange(View v, boolean hasFocus) {
-//
-//        for (int i=0;i<mClParent.getChildCount();i++){
-//            scaleSmall(mClParent.getChildAt(i));
-//        }
-//        if (hasFocus){
-//            scaleBig(v);
-//       }
-//    }
-//
-//    private void scaleBig(View view) {
-//        ObjectAnimator animator1 = ObjectAnimator.ofFloat(view, "scaleX", 1f, 1.1f);
-//        ObjectAnimator animator2 = ObjectAnimator.ofFloat(view, "scaleY", 1f, 1.1f);
-//        AnimatorSet set = new AnimatorSet();
-//        set.playTogether(animator1, animator2);
-//        set.setDuration(0);
-//        if (!set.isRunning()) {
-//            set.start();
-//        }
-//
-//    }
-//
-//    private void scaleSmall(View view) {
-//        ObjectAnimator animator1 = ObjectAnimator.ofFloat(view, "scaleX", 1.1f, 1f);
-//        ObjectAnimator animator2 = ObjectAnimator.ofFloat(view, "scaleY", 1.1f, 1f);
-//        AnimatorSet set = new AnimatorSet();
-//        set.playTogether(animator1, animator2);
-//        set.setDuration(0);
-//        if (!set.isRunning()) {
-//            set.start();
-//        }
-//    }
+    @Override
+    public void onClick(View v) {
+        v.requestFocus();
+        Intent intent = new Intent(MainActivity.this, RecyclerActivity.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/codec/axun/com/tvdemo/RecyclerActivity.java
@@ -0,0 +1,18 @@
+package codec.axun.com.tvdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2018/5/24.
+ */
+
+public class RecyclerActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+    }
+}
